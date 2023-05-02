384d90b4d07e44ba11548627200d7825b0130678
leonardocordeir0 <leonardocordeiroroncada@gmail.com>
2023-05-02 17:21:16 -0300

    modulo3

---
+++ Fundamentos/Módulo-3/Dia 01: JavaScript - Primeiros passos/parafixar.js
@@ -0,0 +1,4 @@
+let myName = "Leo"
+let birthCity = "Maringa"
+let birthYear = "2000"
+console.log(myName,birthCity,birthYear)
---
+++ Fundamentos/Módulo-3/Dia 01: JavaScript - Primeiros passos/parafixar3.js
@@ -0,0 +1,13 @@
+let Notacandidato = 90
+
+if(Notacandidato >= 80){
+    console.log("Parabéns, você faz parte do grupo de pessoas aprovadas!")
+}
+
+else if(Notacandidato <80 && Notacandidato >= 60) {
+    console.log ("Você está na nossa lista de espera.")
+}
+
+else if (Notacandidato <60){
+    console.log ("Infelizmente, você reprovou.")
+}
