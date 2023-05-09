5e706c6914a2d6f2ea2bfac321a1f85eb14eb3de
leonardocordeir0 <leonardocordeiroroncada@gmail.com>
2023-05-09 15:29:41 -0300

    dia04 incial

---
+++ Fundamentos/Módulo-3/Dia 04: JavaScript - Objetos/parafixar.js
@@ -0,0 +1,19 @@
+let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge',
+  };
+
+  for(let index in names){
+    console.log("Olá" + names[index])
+
+    
+  }
+
+  let object = { a: 1, b: 2, c: 3 };
+
+for (let property in object) {
+  console.log(property + ' = ' + object[property]);
+}
+  
+  
